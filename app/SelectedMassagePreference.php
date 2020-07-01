678d38555c1b5b0ba53ea9848b0ed8e993923fbd
JaydeepMor <it.jaydeep.mor@gmail.com>
2020-07-01 12:21:57 +0530

    Update massage preference post (save) API for user module.

--- app/SelectedMassagePreference.php
+++ app/SelectedMassagePreference.php
@@ -15,7 +15,7 @@ class SelectedMassagePreference extends BaseModel
         'user_id'
     ];
 
-    public $radioOptions = [1, 2, 3, 4];
+    public $radioOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     public function validator(array $data)
     {
